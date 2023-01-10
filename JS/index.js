2987e0c7195e834bdb992b533d8cf856de5277cf
Jittrapol Int <jittrapol.i@psu.ac.th>
2023-01-10 14:19:59 +0700

    created index.js

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        let currentScrollTop = 0;
+
+        console.debug("current scroll position: " + currentScrollTop);
+    });
+});
