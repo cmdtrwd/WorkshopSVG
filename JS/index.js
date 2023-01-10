9a4ee2b1fbe0cf20c6c19959f2e6e5bafd3a7c41
Jittrapol Int <jittrapol.i@psu.ac.th>
2023-01-10 14:33:16 +0700

    transparent background when scrolling to the top

--- JS/index.js
+++ JS/index.js
@@ -7,13 +7,25 @@ $(document).ready(function(){
 
         //Scrolling down is detected!
         if (currentScrollTop > lastScrollTop){
-            console.debug("scrolling down");
+            console.debug("scrolling down");      
+            if($("#top-nav").hasClass("active")){
+                $("#top-nav").removeClass("active");
+            }      
         }
         //Scrolling up is detected!
         else{
             console.debug("scrolling up");
+            $("#top-nav").addClass("active");
         }
 
         lastScrollTop = currentScrollTop;
+
+        //The top nav background will become transparent when scrolling to the top of the screen
+        if (currentScrollTop < 1){
+            $("#top-nav").addClass("top");
+        }
+        else{
+            $("#top-nav").removeClass("top");
+        }
     });
 });
